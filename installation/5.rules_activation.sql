c8a9eb8d5568c2821c98be0fd89d136ee3006160
Pavel-Gildt <pavel.gildt@gmail.com>
2023-02-10 12:00:57 +0500

    Create 5.rules_activation.sql

---
+++ installation/5.rules_activation.sql
@@ -0,0 +1,15 @@
+update rule set is_enabled = 1 where upper(rule) = 'ACT_ACTION_COMMENTS TO CHAT';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_DAYS_UNTIL_DUE CFU';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_ACTION_COMMENTS';
+update rule set is_enabled = 1 where upper(rule) = 'ACTION ITEM NOTIFICATION NIGHTLY';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_TIME_LAST_UPDATE FB';
+update rule set is_enabled = 1 where upper(rule) = 'CHAT TO ACT_ACTION_COMMENTS';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_AGE_OF_LAST_UPDATE 15 MINUTES';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_ACTION_DUE NIGHTLY';
+update rule set is_enabled = 1 where upper(rule) = 'ACTION ITEM NOTIFICATION NIGHTLY FB';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_AGE_OF_LAST_UPDATE CFU';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_AGE_OF_ACTION_ITEM CFU';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_ACTION_DUE CFU';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_DAYS_UNTIL_DUE NIGHTLY';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_TIME_LAST_UPDATE';
+update rule set is_enabled = 1 where upper(rule) = 'ACT_AGE_OF_ACTION_ITEM 15 MINUTES';
